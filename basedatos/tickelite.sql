62cf49e31b49035850e77bfe8357b41afeedad6b
valeOsorioSantana <valeosoriosantana0@gmail.com>
2025-05-06 10:41:10 -0500

    comit06052025

--- basedatos/tickelite.sql
+++ basedatos/tickelite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-05-2025 a las 15:29:56
+-- Tiempo de generación: 06-05-2025 a las 17:35:07
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -49,6 +49,43 @@ INSERT INTO `administrador` (`idAdministrador`, `documento`, `nombres`, `telefon
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `calendario`
+--
+
+CREATE TABLE `calendario` (
+  `idCalendario` int(10) NOT NULL,
+  `nomEvent` varchar(255) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `fechaInicio` datetime NOT NULL,
+  `fechaFin` datetime NOT NULL,
+  `ubicacion` varchar(255) NOT NULL,
+  `idUsuario` int(10) NOT NULL,
+  `categoria` varchar(100) NOT NULL,
+  `opcionesEntrada` varchar(100) NOT NULL,
+  `recordatorios` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evento`
+--
+
+CREATE TABLE `evento` (
+  `idEvento` int(10) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `fechaInicio` datetime NOT NULL,
+  `fechaFin` datetime NOT NULL,
+  `ubicacion` varchar(255) NOT NULL,
+  `categoria` varchar(100) NOT NULL,
+  `organizadorId` int(10) NOT NULL,
+  `recordatorios` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -70,7 +107,8 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`idUsuario`, `documento`, `nombres`, `telefono`, `correo`, `contrasena`, `rol`, `estado`) VALUES
 (1, '12345678', 'pepito perez perez', '3002222222', 'pepito@gmail.com', '12345', 'Cliente', 'Activo'),
 (2, '12345679', 'fulanito mazo mazo', '3002222221', 'fulanito@gmail.com', '12345', 'Cliente', 'Activo'),
-(3, '12345677', 'Juanita Perez Mazo', '3002222223', 'juanita@gmail.com', '12345678', 'Cliente', 'Activo');
+(3, '12345677', 'Juanita Perez Mazo', '3002222223', 'juanita@gmail.com', '12345678', 'Cliente', 'Activo'),
+(4, '1234567887', 'olivia rios plao', '3004598756', 'olivia@gmail.com', '$2b$10$nF1HplQvFATVfKFDqQxeAuVCrTCxnVMAMY3jUPyMyipPGxzyAsuwm', 'Cliente', 'Activo');
 
 --
 -- Índices para tablas volcadas
@@ -84,6 +122,19 @@ ALTER TABLE `administrador`
   ADD UNIQUE KEY `documento` (`documento`),
   ADD UNIQUE KEY `telefono` (`telefono`),
   ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- Indices de la tabla `calendario`
+--
+ALTER TABLE `calendario`
+  ADD PRIMARY KEY (`idCalendario`),
+  ADD UNIQUE KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indices de la tabla `evento`
+--
+ALTER TABLE `evento`
+  ADD PRIMARY KEY (`idEvento`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -105,10 +156,22 @@ ALTER TABLE `administrador`
   MODIFY `idAdministrador` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT de la tabla `calendario`
+--
+ALTER TABLE `calendario`
+  MODIFY `idCalendario` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `evento`
+--
+ALTER TABLE `evento`
+  MODIFY `idEvento` int(10) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
